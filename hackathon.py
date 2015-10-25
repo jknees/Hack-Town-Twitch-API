4d2d4f6d21d6c27363cb32931e75093646f34150
Brandon Sov <brandonsov@gmail.com>
2015-10-25 00:42:54 -0700

    Added many functions to switch pages

--- hackathon.py
+++ hackathon.py
@@ -13,10 +13,10 @@ for i in range(4):
 
 devs_choice = {'Dark Souls': 'Dark%20Souls'}
 dev_data = json.loads(requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(devs_choice['Dark Souls'])).text)
-game1_data = requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[0].replace(' ', '%20')))
-game2_data = requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[1].replace(' ', '%20')))
-game3_data = requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[2].replace(' ', '%20')))
-game4_data = requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[3].replace(' ', '%20')))
+game1_data = json.loads(requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[0].replace(' ', '%20'))).text)
+game2_data = json.loads(requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[1].replace(' ', '%20'))).text)
+game3_data = json.loads(requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[2].replace(' ', '%20'))).text)
+game4_data = json.loads(requests.get('https://api.twitch.tv/kraken/streams?game={}&limit=1'.format(games[3].replace(' ', '%20'))).text)
 #data = json.loads(dev_data.text)
 
 def game_details(data):
@@ -28,12 +28,23 @@ def game_details(data):
                 'Logo':data['streams'][0]['channel']['logo'],\
                 'Status':data['streams'][0]['channel']['status']}
     return details
-
 @app.route("/")
+@app.route("/index.html")
+@app.route("/index")
 def index():
     """ Displays the index page accessible at '/'
     """
-    return render_template('index.html', streamer = game_details(dev_data)['Display Name'],\
+    return render_template('index.html', Game1 = games[0],\
+                                         Game2 = games[1],\
+                                         Game3 = games[2],\
+                                         Game4 = games[3]
+                                         )
+@app.route("/top_pick.html")
+@app.route("/top_pick")
+def top_pick():
+    """ Displays the top_pick page accessible at '/'
+    """
+    return render_template('top_pick.html', streamer = game_details(dev_data)['Display Name'],\
                                          channel = game_details(dev_data)['Display Name'], \
                                          Stream = game_details(dev_data)['Status'], \
                                          picture = game_details(dev_data)['Logo'],\
@@ -43,13 +54,60 @@ def index():
                                          Game4 = games[3]
                                          )
 
-def top_pick():
-    """ Displays the index page accessible at '/'
+@app.route("/game1.html")
+@app.route("/game1")
+def game1():
+    """ Displays the top_pick page accessible at '/'
     """
-    return render_template('top_pick.html', streamer = game_details(dev_data)['Display Name'],\
-                                         channel = game_details(dev_data)['Display Name'], \
-                                         Stream = game_details(dev_data)['Status'], \
-                                         picture = game_details(dev_data)['Logo'],\
+    return render_template('game1.html', streamer = game_details(game1_data)['Display Name'],\
+                                         channel = game_details(game1_data)['Display Name'], \
+                                         Stream = game_details(game1_data)['Status'], \
+                                         picture = game_details(game1_data)['Logo'],\
+                                         Game1 = games[0],\
+                                         Game2 = games[1],\
+                                         Game3 = games[2],\
+                                         Game4 = games[3]
+                                         )
+
+@app.route("/game2.html")
+@app.route("/game2")
+def game2():
+    """ Displays the top_pick page accessible at '/'
+    """
+    return render_template('game2.html', streamer = game_details(game2_data)['Display Name'],\
+                                         channel = game_details(game2_data)['Display Name'], \
+                                         Stream = game_details(game2_data)['Status'], \
+                                         picture = game_details(game2_data)['Logo'],\
+                                         Game1 = games[0],\
+                                         Game2 = games[1],\
+                                         Game3 = games[2],\
+                                         Game4 = games[3]
+                                         )
+
+@app.route("/game3.html")
+@app.route("/game3")
+def game3():
+    """ Displays the top_pick page accessible at '/'
+    """
+    return render_template('game3.html', streamer = game_details(game3_data)['Display Name'],\
+                                         channel = game_details(game3_data)['Display Name'], \
+                                         Stream = game_details(game3_data)['Status'], \
+                                         picture = game_details(game3_data)['Logo'],\
+                                         Game1 = games[0],\
+                                         Game2 = games[1],\
+                                         Game3 = games[2],\
+                                         Game4 = games[3]
+                                         )
+
+@app.route("/game4.html")
+@app.route("/game4")
+def game4():
+    """ Displays the top_pick page accessible at '/'
+    """
+    return render_template('game4.html', streamer = game_details(game4_data)['Display Name'],\
+                                         channel = game_details(game4_data)['Display Name'], \
+                                         Stream = game_details(game4_data)['Status'], \
+                                         picture = game_details(game4_data)['Logo'],\
                                          Game1 = games[0],\
                                          Game2 = games[1],\
                                          Game3 = games[2],\
